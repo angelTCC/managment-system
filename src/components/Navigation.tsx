0657bf9a5e96f1bca80001e9e38b5850af4d570d
angel <angel20170238b@gmail.com>
2026-06-05 19:02:31 -0500

    layout ready

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faListCheck } from "@fortawesome/free-solid-svg-icons";
 
 interface NavLink {
@@ -32,28 +33,38 @@ export default function Navigation() {
   const pathname = usePathname();
 
   return (
-    <nav>
-      <ul>
-        {menuItems.map((item) => {
-          const isActive = pathname === item.href;
+    <>
+      {/* ==== LOGO ==== */}
+      <div style={{ background: "red" }}>
+        <h1>Manager System</h1>
+      </div>
 
-          return (
-            <li key={item.href}>
-              <Link
-                href={item.href}
-                className={cn(
-                  "group flex items-center gap-3 rounded-lg px-3 py-3 text-sm font-medium transition-all",
-                  // Estilos: Hover azul, Activo azul sólido
-                  "hover:bg-[#174681] hover:text-white text-gray-700",
-                  isActive && "bg-[#174681] text-white shadow-md",
-                )}
-              >
-                {item.label}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+      {/*==== NAVIGATION ==== */}
+      <nav style={{ backgroundColor: "yellow" }}>
+        <ul>
+          {menuItems.map((item) => {
+            const isActive = pathname === item.href;
+
+            return (
+              <li key={item.href}>
+                <Link
+                  href={item.href}
+                  className={cn(
+                    "block rounded-lg px-3 py-3 flex",
+                    // Estilos: Hover azul, Activo azul sólido
+                    //group flex items-center gap-3 rounded-lg px-3 py-3 text-sm font-medium transition-all
+                    "hover:bg-[#174681] hover:text-white",
+                    isActive && "bg-[#174681] text-white",
+                  )}
+                >
+                  <FontAwesomeIcon icon={item.icon} />
+                  <p>{item.label}</p>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
   );
 }
